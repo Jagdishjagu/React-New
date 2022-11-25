508eeba03f109a71a8470719e82ed3e4f9885496
Jagdishjagu <Jagujagdish199@gmail.com>
2022-11-25 13:49:33 +0530

    okie

---
+++ react-2/src/Navbar/Navbar.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg navbar-white bg-dark">
+        <div className="container ">
+          <img
+            src="https://ekartlogistics.com/assets/images/ekblueLogo.png"
+            width={100}
+            alt="#"
+          />
+          <ul className="navbar-nav ">
+            <li className="nav-item">
+              <Link to="/home" className="nav-link">
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/service" className="nav-link">
+                Services
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/product" className="nav-link">
+                Product
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/about" className="nav-link">
+                About
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/contact" className="nav-link">
+                Contact
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
