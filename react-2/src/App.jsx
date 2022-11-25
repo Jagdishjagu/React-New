41c2462ea6684c0c5023d43f67dbf9b552690f7a
Jagdishjagu <Jagujagdish199@gmail.com>
2022-11-25 20:59:27 +0530

    okie

--- react-2/src/App.jsx
+++ react-2/src/App.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import Navbar from "./Navbar/Navbar";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
-import Service from "./Pages/Service";
-import Product from "./Pages/Product";
-import About from "./Pages/About";
-import Contact from "./Pages/Contact";
+import Service from "./Pages/Services/Service";
+import Product from "./Pages/Product/Product";
+import About from "./Pages/About/About";
+import Contact from "./Pages/Contact/Contact";
 
 const App = () => {
   return (
